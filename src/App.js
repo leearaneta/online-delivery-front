54867509939c954304ffb1cd1ae79cc7c527462f
greatermeans <gakouri@gmail.com>
2016-09-12 09:20:45 -0400

    there is an error somewhere

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import AppContainer from './containers/AppContainer';
-import 'bootstrap/less/bootstrap.less';
 
 export default class App extends Component {
   render() {
