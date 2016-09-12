9dbe9c50e73fc8e7baf2950096f15f9e305a1f69
greatermeans <gakouri@gmail.com>
2016-09-12 09:03:18 -0400

    search bar is functional, working on bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AppContainer from './containers/AppContainer';
+import 'bootstrap/less/bootstrap.less';
 
 export default class App extends Component {
   render() {
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,29 +1,22 @@
 import React, { Component } from 'react';
 import Geosuggest from 'react-geosuggest';
 
-class Landing extends Component {
+export default class Landing extends Component {
 
 	 onSuggestSelect(suggest) {
     console.log(suggest);
   }
     
 	render(){
-		debugger
-		const fixtures = [
-      {label: 'Old Elbe Tunnel, Hamburg', location: {lat: 53.5459, lng: 9.966576}},
-      {label: 'Reeperbahn, Hamburg', location: {lat: 53.5495629, lng: 9.9625838}},
-      {label: 'Alster, Hamburg', location: {lat: 53.5610398, lng: 10.0259135}}
-    ];
 
     return (
       <div id="hel">
         <Geosuggest
-          placeholder="Start typing!"
-          initialValue="Hamburg"
-          fixtures={fixtures}
+          placeholder="Where do you want it delivered?"
           onSuggestSelect={this.onSuggestSelect}
-          location={new google.maps.LatLng(53.558572, 9.9278215)}
-          radius="20" />
+          location={new google.maps.LatLng(62.2315, 16.1932)}
+          country="SE"
+          radius="1000" />
       </div>
     )
   }
